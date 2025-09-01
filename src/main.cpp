8af817382667e6cb59007dc2bfbca3b44edc461e
yozaman-coder <jamesstraka5@gmail.com>
2025-09-01 00:54:48 -0600

    git ignore compile_commands.json

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include <print>
+//#include <print>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::println("Hello World!");
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	return 0;
 }
